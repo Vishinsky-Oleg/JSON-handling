5d3f60e32b08a7afdd43df2e5e45da57a047175a
Vishinsky-Oleg <vishinsky.oleg@mail.com>
2020-11-16 22:32:28 +0300

    Sorting json file

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,178 +1,183 @@
-(function () {
-    'use strict'
+function formWrapper() {
+    (function () {
+        'use strict'
 
-    window.addEventListener('load', function () {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation')
+        window.addEventListener('load', function () {
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.getElementsByClassName('needs-validation')
 
-        // Loop over them and prevent submission
-        Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
+            // Loop over them and prevent submission
+            Array.prototype.filter.call(forms, function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (form.checkValidity() === false) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
 
-                form.classList.add('was-validated')
-            }, false)
-        })
-    }, false)
-})()
+                    form.classList.add('was-validated')
+                }, false)
+            })
+        }, false)
+    })()
 
 
-class Item {
-    constructor(name, price, isnew, imagePath) {
-        this.name = name;
-        this.price = price;
-        this.isNew = isnew;
-        this.imagePath = imagePath;
+    class Item {
+        constructor(name, price, isnew, imagePath) {
+            this.name = name;
+            this.price = price;
+            this.isNew = isnew;
+            this.imagePath = imagePath;
+        }
     }
+
+
+    const myForm = document.querySelector('.myForm');
+    const myName = document.querySelector('#firstName');
+    const myPrice = document.querySelector('#lastName');
+    const myIsNew = document.querySelector('#is-new');
+    const myImageName = document.querySelector('#file');
+
+    myForm.addEventListener('submit', function (e) {
+        // console.log(myName.value);
+        // console.log(myPrice.value);
+        // if (myIsNew.checked) {
+        //     console.log(true);
+        // } else {
+        //     console.log(false);
+        // }
+
+        // for (file of myImageName.files) {
+        //     console.log(file.name);
+
+        // };
+        e.preventDefault();
+        let fileNames = [];
+        for (file of myImageName.files) {
+            fileNames.push(file.name);
+        }
+        console.log(JSON.stringify(new Item(myName.value, myPrice.value, myIsNew.checked, fileNames)));
+        // console.log(new Item(myName.value, myPrice.value, myIsNew.checked, fileNames));
+
+
+
+    })
+    myImageName.addEventListener('change', function () {
+        for (file of myImageName.files) {
+            const image = document.createElement('img');
+            image.src = URL.createObjectURL(file);
+            document.querySelector('.append').appendChild(image);
+        };
+    })
+
+    myImageName.addEventListener('click', function () {
+        let images = document.querySelectorAll('.append img');
+        images.forEach(img => {
+            img.parentNode.removeChild(img);
+        });
+    })
 }
-
-
-const myForm = document.querySelector('.myForm');
-const myName = document.querySelector('#firstName');
-const myPrice = document.querySelector('#lastName');
-const myIsNew = document.querySelector('#is-new');
-const myImageName = document.querySelector('#file');
-
-myForm.addEventListener('submit', function (e) {
-    // console.log(myName.value);
-    // console.log(myPrice.value);
-    // if (myIsNew.checked) {
-    //     console.log(true);
-    // } else {
-    //     console.log(false);
-    // }
-
-    // for (file of myImageName.files) {
-    //     console.log(file.name);
-
-    // };
-    e.preventDefault();
-    let fileNames = [];
-    for (file of myImageName.files) {
-        fileNames.push(file.name);
-    }
-    console.log(JSON.stringify(new Item(myName.value, myPrice.value, myIsNew.checked, fileNames)));
-    // console.log(new Item(myName.value, myPrice.value, myIsNew.checked, fileNames));
-
-
-
-})
-myImageName.addEventListener('change', function () {
-    for (file of myImageName.files) {
-        const image = document.createElement('img');
-        image.src = URL.createObjectURL(file);
-        document.querySelector('.append').appendChild(image);
-    };
-})
-
-myImageName.addEventListener('click', function () {
-    let images = document.querySelectorAll('.append img');
-    images.forEach(img => {
-        img.parentNode.removeChild(img);
-    });
-})
-
 const items = document.querySelector('.items');
 
 // let itemsJson;
 
 const container = document.querySelector('.items');
-fetch('myjsonfile.json').then(function (response) {
-    if (response.ok) {
-        response.json().then(function (json) {
-            products = json;
-            for (item of products) {
-                let itemWrapper = document.createElement('div');
-                let header = document.createElement('h1');
-                let para = document.createElement('p');
-                let inputWrapper = document.createElement('div');
-                let minusBtn = document.createElement('span');
-                let input = document.createElement('input');
-                let plusBtn = document.createElement('span');
-                inputWrapper.classList.add('order-count');
-                minusBtn.classList.add('input-change');
-                minusBtn.textContent = '-';
-                minusBtn.addEventListener('click', () => {
-                    if (minusBtn.nextElementSibling.value != '1') {
-                        minusBtn.nextElementSibling.value = parseInt(minusBtn.nextElementSibling.value) - 1;
-                    }
-                })
-                plusBtn.classList.add('input-change');
-                plusBtn.textContent = '+';
 
-                plusBtn.addEventListener('click', () => {
-                    plusBtn.previousElementSibling.value = parseInt(plusBtn.previousElementSibling.value) + 1;
-                })
+function fetchWrapper() {
+    fetch('myjsonfile.json').then(function (response) {
+        if (response.ok) {
+            response.json().then(function (json) {
+                products = json;
+                for (item of products) {
+                    let itemWrapper = document.createElement('div');
+                    let header = document.createElement('h1');
+                    let para = document.createElement('p');
+                    let inputWrapper = document.createElement('div');
+                    let minusBtn = document.createElement('span');
+                    let input = document.createElement('input');
+                    let plusBtn = document.createElement('span');
+                    inputWrapper.classList.add('order-count');
+                    minusBtn.classList.add('input-change');
+                    minusBtn.textContent = '-';
+                    minusBtn.addEventListener('click', () => {
+                        if (minusBtn.nextElementSibling.value != '1') {
+                            minusBtn.nextElementSibling.value = parseInt(minusBtn.nextElementSibling.value) - 1;
+                        }
+                    })
+                    plusBtn.classList.add('input-change');
+                    plusBtn.textContent = '+';
 
-                input.type = 'number';
-                input.value = 1;
-                input.min = 1;
-                input.setAttribute('readonly', '');
-                inputWrapper.appendChild(minusBtn);
-                inputWrapper.appendChild(input);
-                inputWrapper.appendChild(plusBtn);
-                let button = document.createElement('button');
-                header.textContent = item.name;
-                para.textContent = item.price;
-                button.textContent = "BUY";
-                button.value = item.id;
+                    plusBtn.addEventListener('click', () => {
+                        plusBtn.previousElementSibling.value = parseInt(plusBtn.previousElementSibling.value) + 1;
+                    })
+
+                    input.type = 'number';
+                    input.value = 1;
+                    input.min = 1;
+                    input.setAttribute('readonly', '');
+                    inputWrapper.appendChild(minusBtn);
+                    inputWrapper.appendChild(input);
+                    inputWrapper.appendChild(plusBtn);
+                    let button = document.createElement('button');
+                    header.textContent = item.name;
+                    para.textContent = item.price;
+                    button.textContent = "BUY";
+                    button.value = item.id;
 
 
-                itemWrapper.appendChild(header);
-                itemWrapper.appendChild(para);
-                itemWrapper.appendChild(inputWrapper);
-                itemWrapper.appendChild(button);
-                container.appendChild(itemWrapper);
+                    itemWrapper.appendChild(header);
+                    itemWrapper.appendChild(para);
+                    itemWrapper.appendChild(inputWrapper);
+                    itemWrapper.appendChild(button);
+                    container.appendChild(itemWrapper);
 
-                button.addEventListener('click', () => {
-                    let inputValue = parseInt(button.previousSibling.children[1].value);
-                    if (localStorage.order) {
-                        console.log(inputValue);
-                        if (inputValue > 1) {
-                            localStorage.order += ',' + button.value + 'x' + inputValue;
+                    button.addEventListener('click', () => {
+                        let inputValue = parseInt(button.previousSibling.children[1].value);
+                        if (localStorage.order) {
+                            console.log(inputValue);
+                            if (inputValue > 1) {
+                                localStorage.order += ',' + button.value + 'x' + inputValue;
+
+                            } else {
+                                localStorage.order += ',' + button.value;
+                            }
 
                         } else {
-                            localStorage.order += ',' + button.value;
+                            console.log(inputValue);
+                            if (inputValue > 1) {
+                                localStorage.setItem('order', `${button.value}x${inputValue}`);
+
+                            } else {
+                                localStorage.setItem('order', `${button.value}`);
+                            }
                         }
 
-                    } else {
-                        console.log(inputValue);
-                        if (inputValue > 1) {
-                            localStorage.setItem('order', `${button.value}x${inputValue}`);
-
-                        } else {
-                            localStorage.setItem('order', `${button.value}`);
-                        }
-                    }
-
-                });
-            }
-        });
-    } else {
-        console.log(
-            "Network request for products.json failed with response " +
-            response.status +
-            ": " +
-            response.statusText
-        );
-    }
-});
-
+                    });
+                }
+            });
+        } else {
+            console.log(
+                "Network request for products.json failed with response " +
+                response.status +
+                ": " +
+                response.statusText
+            );
+        }
+    });
+}
 //FOR ALREADY EXISTING BUTTONS
-const inputButMinus = document.querySelector('.input-change-minus');
-inputButMinus.addEventListener('click', () => {
-    if (inputButMinus.nextElementSibling.value != '1') {
-        inputButMinus.nextElementSibling.value = parseInt(inputButMinus.nextElementSibling.value) - 1;
-    }
-})
+function wrapper2() {
+    const inputButMinus = document.querySelector('.input-change-minus');
+    inputButMinus.addEventListener('click', () => {
+        if (inputButMinus.nextElementSibling.value != '1') {
+            inputButMinus.nextElementSibling.value = parseInt(inputButMinus.nextElementSibling.value) - 1;
+        }
+    })
 
-const inputButPlus = document.querySelector('.input-change-plus');
-inputButPlus.addEventListener('click', () => {
-    inputButPlus.previousElementSibling.value = parseInt(inputButMinus.nextElementSibling.value) + 1;
-})
+    const inputButPlus = document.querySelector('.input-change-plus');
+    inputButPlus.addEventListener('click', () => {
+        inputButPlus.previousElementSibling.value = parseInt(inputButMinus.nextElementSibling.value) + 1;
+    })
+}
 // _______________________________________
 
 // let ordered = ['2222', '3333', '1111', '2111', '1234', '3333x5', '2222x3', '2222', '2222', '2111x3', '2111', '2222'];
@@ -218,9 +223,11 @@ function orderToObject(orderStr) {
     return fullOrder; //RETURN OBJECT WITH KEY/VALUE ID/COUNT FROM TAKEN STRING
 }
 
+
 // count(ordered);
 
-// let oo = orderToObject(localStorage.order);
+
+
 // for (let i in oo) {
 //     console.log(i + ' ' + oo[i]);
 // }
@@ -256,3 +263,74 @@ function countElementsInArray(array) {
     });
     return count;
 }
+
+function fetchingJSONandGetItemsByID() {
+    fetch('myjsonfile.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Something went wrong');
+            } else {
+                return response.json()
+            }
+        })
+        .then(jsonFile => {
+            for (item of jsonFile) {
+                if (item.id === '4444')
+                    console.log(item);
+            }
+        })
+        .catch(error => {
+            console.log('There has been an:' + error);
+        });
+}
+
+function functionsExecutedRightAway() {
+    (function () {
+        console.log('HI MY NAME IS JEFF');
+    })();
+
+    (async () => {
+        let response = await fetch('myjsonfile.json');
+        let jsonFile = await response.json();
+        console.log(jsonFile);
+    })();
+}
+
+
+
+const mainHeader = document.querySelector('.main-header');
+// console.log(mainHeader.textContent);
+
+
+async function sortBy(string) {
+    let response = await fetch('myjsonfile.json');
+    let jsonFile = await response.json();
+    if (string === 'Drinks') {
+        for (item of jsonFile) {
+            if (item.type === 'drinks') {
+                let itemWrapper = document.createElement('div');
+                let para = document.createElement('p');
+                para.textContent = item.name;
+                itemWrapper.appendChild(para);
+                container.appendChild(itemWrapper);
+
+            }
+        }
+    } else if (string === 'Order') {
+        let oo = orderToObject(localStorage.order);
+        for (let i in oo) {
+            for (item of jsonFile) {
+                if (item.id === i) {
+                    let itemWrapper = document.createElement('div');
+                    let para = document.createElement('p');
+                    para.textContent = oo[i] + ' items of ' + item.name + ' cost ' + (oo[i] * item.price) + '$ each is ' + item.price;
+                    itemWrapper.appendChild(para);
+                    container.appendChild(itemWrapper);
+                }
+            }
+        }
+
+    }
+}
+
+sortBy(mainHeader.textContent);
